14fe31441d9216e17ad7ccae8515cd5d0bee4d64
Sergey Zhukov <sergey@video-gloria.mshanken.com>
2014-01-23 12:11:22 -0500

    making some updates

--- memcached-master/metadata.rb
+++ memcached-master/metadata.rb
@@ -7,8 +7,6 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.7.1'
 
 depends           'runit', '~> 1.0'
-depends           'yum', '~> 3.0'
-depends           'yum-epel'
 
 supports 'amazon'
 supports 'centos'
