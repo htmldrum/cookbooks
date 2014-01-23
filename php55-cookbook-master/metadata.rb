f45529ecd16f1e2f5cd6d04ff33bb05a6e2100a8
Sergey Zhukov <sergey@video-gloria.mshanken.com>
2014-01-23 12:20:13 -0500

    releasing this cookbook of soem dependencies... for now

--- php55-cookbook-master/metadata.rb
+++ php55-cookbook-master/metadata.rb
@@ -5,8 +5,5 @@ license           "Apache 2.0"
 description      "Installs and php 5.5"
 version          "0.0.2"
 
-depends "build-essential"
-depends "apt"
-depends "yum"
 
 recipe "php55", "Installs php"
