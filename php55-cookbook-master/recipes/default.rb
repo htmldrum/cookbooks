1b11919d0f72926a96ec6d02c57938a5b4a2b63a
Sergey Zhukov <sergey@dbayer.mshanken.com>
2014-01-29 15:36:47 -0500

    adding soem helpfulnes to ubuntu deps.

--- php55-cookbook-master/recipes/default.rb
+++ php55-cookbook-master/recipes/default.rb
@@ -78,3 +78,23 @@ case node[:platform]
 	end
 	
   end
+
+ when "ubuntu"
+    include_recipe "apt"
+
+    execute "python-software-properties" do 
+      command "apt-get install -y python-software-properties"
+      action :run
+    end
+
+    execute "install php55" do
+      #command "apt-get install -y php5"
+      command "add-apt-repository ppa:ondrej/php5 && apt-get update -y && apt-get install -y php5 php5-mcrypt php5-json"
+      action :run
+    end
+
+    execute "restart apache2" do
+      #command "apt-get install -y php5"
+      command "service apache2 restart"
+      action :run
+    end
