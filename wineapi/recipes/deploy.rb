bba5e5f20980b9e16a64a132606f04d94565b8fd
Cron Jeremy <szhukov@mshanken.com>
2014-01-31 12:53:37 -0500

    forgot some quotes.

--- wineapi/recipes/deploy.rb
+++ wineapi/recipes/deploy.rb
@@ -1,7 +1,7 @@
 # Deploys the given git project to the deploy directory.
 git "/var/www" do
   repository "git@github.com:mshanken/winedb-api.git"
-  revision devstable
+  revision "devstable"
   action :sync
   user "root"
   group "root"
