53d30958f0d0c45542c4b4d865d8c9ff8b2a03d8
Cron Jeremy <szhukov@mshanken.com>
2014-01-31 15:35:39 -0500

    even more changes to make it work.

--- wineapi/recipes/deploy.rb
+++ wineapi/recipes/deploy.rb
@@ -1,5 +1,5 @@
 # Deploys the given git project to the deploy directory.
-git "/var/www" do
+git "/var/www/wineapi/" do
   repository "git@github.com:mshanken/winedb-api.git"
   revision "devstable"
   action :sync
@@ -10,14 +10,14 @@ end
 
 # Creates log and cache dirs for the Kohana project.
 
-directory "/var/www/application/logs" do
+directory "/var/www/wineapi/application/logs" do
   recursive true
   owner "www-data"
   group "www-data"
   mode "777"
 end
 
-directory "/var/www/application/cache" do
+directory "/var/www/wineapi/application/cache" do
   recursive true
   owner "www-data"
   group "www-data"
