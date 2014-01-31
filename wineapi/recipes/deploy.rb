2422d169afd7b66eeffa2d3203f1ec87ace3b6b5
Sergey Zhukov <sergey@dbayer.mshanken.com>
2014-01-31 12:47:48 -0500

    phuxing with the deploy recipe.

--- wineapi/recipes/deploy.rb
+++ wineapi/recipes/deploy.rb
@@ -1,7 +1,7 @@
 # Deploys the given git project to the deploy directory.
 git "/var/www" do
-  repository "https://github.com:mshanken/winedb-api.git"
-  revision "devstable"
+  repository "git@github.com:mshanken/winedb-api.git"
+  revision devstable
   action :sync
   user "root"
   group "root"
