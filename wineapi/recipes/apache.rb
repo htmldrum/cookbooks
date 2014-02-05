91fd198898b3663efb94ae084a5a2c19b032df34
Cron Jeremy <szhukov@mshanken.com>
2014-02-05 11:07:24 -0500

    more config steps for apache to like me...

--- wineapi/recipes/apache.rb
+++ wineapi/recipes/apache.rb
@@ -5,6 +5,9 @@ template "/etc/apache2/sites-enabled/000-default.conf" do
   mode "644"
 end
 
+execute "a2enmod rewrite" do
+ command "sudo a2enmod rewrite"
+end
 
 service "apache2" do
   action :restart
