bae4223f37a23a69d7b03cd99e2c29344520955b
mturro <mturro@mshanken.com>
2014-02-06 15:05:29 -0500

    adding in winecook for wso - no promises

---
+++ winecook/metadata.rb
@@ -0,0 +1,10 @@
+name             'winecook'
+maintainer       'mturro'
+maintainer_email 'mturro@mshanken.com'
+license          'All rights reserved'
+description      'Installs/Configures winecook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
+depends 'php'
