8ca59200487c31242125ae29b8cc6a70b8ddbdaf
mturro <mturro@mshanken.com>
2014-03-19 15:39:52 -0400

    add post_deploy commands to deploy_opsworks because of directory paths

--- wsocook/recipes/deploy_opsworks.rb
+++ wsocook/recipes/deploy_opsworks.rb
@@ -9,3 +9,37 @@ git "/var/www/wso/" do
   user "root"
   group "root"
 end
+
+remote_file "Copy aws config  file" do
+  path "/usr/share/php/AWSSDKforPHP/config.inc.php"
+  source "file:///var/www/data/config.inc.php"
+  owner 'root'
+  group 'root'
+  mode 0755
+end
+
+%w{/var/www/wso/cache /var/www/wso/log /var/www/wso/content-cache}.each do |mkdir|
+  directory mkdir do
+    mode 0777
+    action :create
+  end
+end
+
+bash "touch content_cache.lock" do
+  code <<-EOH
+  cd /var/www/wso
+  touch content_cache.lock
+  chmod 777 content_cache.lock
+  EOH
+end
+
+bash "fix symfony permissions" do
+  code <<-EOH
+  cd /var/www/wso
+  symfony fix-perms
+  EOH
+end
+
+service "apache2" do
+  action :restart
+end
