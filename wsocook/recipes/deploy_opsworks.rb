3fc50fc65f1a2e9cc84a7ed022fe95a7e89d0e81
mturro <mturro@mshanken.com>
2014-03-19 16:21:07 -0400

    trying to find the inside of the cosmic egg

--- wsocook/recipes/deploy_opsworks.rb
+++ wsocook/recipes/deploy_opsworks.rb
@@ -13,8 +13,8 @@ end
 remote_file "aws_config" do
   path "/usr/share/php/AWSSDKforPHP/config.inc.php"
   source "file:///var/www/wso/data/config.inc.php"
-  owner 'root'
-  group 'root'
+  owner "root"
+  group "root"
   mode 0755
 end
 
