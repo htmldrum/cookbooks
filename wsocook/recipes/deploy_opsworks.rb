cace0dfd6ca14640a6e827a082d771582b42d006
mturro <mturro@mshanken.com>
2014-03-20 11:46:06 -0400

    isolating aws_config to it's own recipe for troubleshooting opsworks

--- wsocook/recipes/deploy_opsworks.rb
+++ wsocook/recipes/deploy_opsworks.rb
@@ -10,14 +10,6 @@ git "/var/www/wso/" do
   group "root"
 end
 
-#remote_file "aws_config" do
-#  path "/usr/share/php/AWSSDKforPHP/config.inc.php"
-#  source "file:///var/www/wso/data/config.inc.php"
-#  owner "root"
-#  group "root"
-#  mode 0755
-#end
-
 %w{/var/www/wso/cache /var/www/wso/log /var/www/wso/content-cache}.each do |mkdir|
   directory mkdir do
     mode 0777
