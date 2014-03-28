61554dd985ed06a2055edfd8558fc1412b2f3583
Cron Jeremy <szhukov@mshanken.com>
2014-03-27 22:07:44 -0400

    fixing attribute syntax...

--- identityapi/recipes/ssl.rb
+++ identityapi/recipes/ssl.rb
@@ -1,17 +1,17 @@
 template "/etc/apache2/ssl/cert.crt" do
     mode 0600
     source 'ssl.key.erb'
-    variables :key => node[:opsworks][:deploy]['identity_api'][:ssl_certificate]
+    variables :key => node[:opsworks]['identity_api'][:ssl_certificate]
     end
 
 template "/etc/apache2/ssl/key.key" do
     mode 0600
     source 'ssl.key.erb'
-    variables :key => node[:opsworks][:deploy]['identity_api'][:ssl_certificate_key]
+    variables :key => node[:opsworks]['identity_api'][:ssl_certificate_key]
     end
 
 template "/etc/apache2/ssl/caauth.ca" do
     mode 0600
     source 'ssl.key.erb'
-    variables :key => node[:opsworks][:deploy]['identity_api'][:ssl_certificate_ca]
+    variables :key => node[:opsworks]['identity_api'][:ssl_certificate_ca]
     end
