7b4983c7eadcc3d25b77f06a37829b584d5422c7
Cron Jeremy <szhukov@mshanken.com>
2014-03-28 12:57:57 -0400

    more apache rules

--- identityapi/recipes/apache.rb
+++ identityapi/recipes/apache.rb
@@ -5,21 +5,23 @@ template "/etc/apache2/sites-enabled/000-default.conf" do
   mode "644"
 end
 
+execute "a2enmod ssl" do
+ command "sudo a2enmod ssl"
+end
 
-template "/etc/apache2/sites-enabled/id.mshanken.com" do
+execute "a2ensite default-ssl.conf" do
+ command "sudo a2ensite default-ssl.conf"
+end
+
+template "/etc/apache2/sites-enabled/default-ssl.conf" do
   source "id.mshanken.comnew.erb"
   owner "root"
   group "root"
   mode "644"
 end
 
-
 execute "a2enmod rewrite" do
  command "sudo a2enmod rewrite"
-end
-
-execute "a2enmod ssl" do
- command "sudo a2enmod ssl"
 end
 
 service "apache2" do
