9fce470c03cc92f524142d4c2edb3862dfdc0669
Cron Jeremy <szhukov@mshanken.com>
2014-03-28 16:23:32 -0400

    adding a deploy recipe for production. Messy but will do for now.

---
+++ identityapi/recipes/deploy_prod.rb
@@ -0,0 +1,72 @@
+# Deploys the given git project to the deploy directory.
+git "/var/www/identity/" do
+  repository "git@github.com:mshanken/identity-api.git"
+  revision "1.5.3"
+  action :sync
+  enable_submodules true
+  user "root"
+  group "root"
+end
+
+
+
+# Creates log and cache dirs for the Kohana project.
+
+directory "/var/www/identity/application/logs" do
+  recursive true
+  owner "www-data"
+  group "www-data"
+  mode "777"
+end
+
+directory "/var/www/identity/application/cache" do
+  recursive true
+  owner "www-data"
+  group "www-data"
+  mode "777"
+end
+
+#Setting db values
+
+template "/var/www/identity/application/config/database.php" do
+  source "database.php.erb"
+  owner "root"
+  group "root"
+  mode "644"
+end
+
+#Setting identity values
+
+template "/var/www/identity/application/config/identity.php" do
+  source "identity.php.erb"
+  owner "root"
+  group "root"
+  mode "644"
+end
+
+#Setting metamodel values
+
+template "/var/www/identity/application/config/metamodel.php" do
+  source "metamodel.php.erb"
+  owner "root"
+  group "root"
+  mode "644"
+end
+
+#Setting metamodeluser values
+
+template "/var/www/identity/application/config/metamodeluser.php" do
+  source "metamodeluser.php.erb"
+  owner "root"
+  group "root"
+  mode "644"
+end
+
+#Setting test values
+
+template "/var/www/identity/application/config/test.php" do
+  source "test.php.erb"
+  owner "root"
+  group "root"
+  mode "644"
+end
