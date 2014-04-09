78f9a90cd5297b3320f3a42c5126b40411779bfb
Cron Jeremy <szhukov@mshanken.com>
2014-04-09 10:41:04 -0400

    adding stagign recipe.

--- identityapi/recipes/deploy_staging.rb
+++ identityapi/recipes/deploy_staging.rb
@@ -1,3 +1,31 @@
+# Deploys the given git project to the deploy directory.
+git "/var/www/identity/" do
+  repository "git@github.com:mshanken/identity-api.git"
+  revision "staging"
+  action :sync
+  enable_submodules true
+  user "root"
+  group "root"
+end
+
+
+
+# Creates log and cache dirs for the Kohana project.
+
+directory "/var/www/identity/application/logs" do
+  recursive true
+  owner "www-data"
+  group "www-data"
+  mode "777"
+end
+
+directory "/var/www/identity/application/cache" do
+  recursive true
+  owner "www-data"
+  group "www-data"
+  mode "777"
+end
+
 #Setting db values
 
 template "/var/www/identity/application/config/database.php" do
