94211441f4e51492dc8568c7a0ed573374db5552
Cron Jeremy <szhukov@mshanken.com>
2014-05-06 17:30:09 -0400

    adding changes for the base cloudsearch template.

--- wineapi/recipes/cloudsearch.rb
+++ wineapi/recipes/cloudsearch.rb
@@ -5,12 +5,3 @@ template "/var/www/wineapi/application/config/cloudsearch.php" do
   mode "644"
 end
 
-execute "cloud search json WGET" do    
-  command "wget --output-document=/tmp/csfile --no-check-certificate https://wineapi.mshanken.com/discover/cloudsearch/domain/production`date +%Y%m%d`"
-  action :run
-end
-
-execute "cloudsearch index create" do
-  command "php /var/www/wineapi/modules/metamodel/tools/cloudsearch/start-domain.php ./tmp/csfile"
-  action :run
-end
