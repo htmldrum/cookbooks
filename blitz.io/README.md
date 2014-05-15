692eb33e2a0fef64c27e13bbcb615a163bbd03e3
Cron Jeremy <szhukov@mshanken.com>
2014-05-15 13:43:44 -0400

    adding blitz.io cookbook.

---
+++ blitz.io/README.md
@@ -0,0 +1,13 @@
+# blitz.io cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ blitz.io/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: blitz.io
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+template "/var/www/wineapi/mu-79d650c0-34ab024a-0c011143-8d223b6f.txt" do
+  source "mu-79d650c0-34ab024a-0c011143-8d223b6f.txt.erb"
+  owner "root"
+  group "root"
+  mode "644"
+end
