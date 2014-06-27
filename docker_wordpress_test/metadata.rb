0950c584632d40b90444d341d02cd62e4cfca4c9
Cron Jeremy <szhukov@mshanken.com>
2014-06-27 10:08:29 -0400

    adding a wordpress depploy recipe withich will be used in prod as well as a test docker_wordpress cookbook.

---
+++ docker_wordpress_test/metadata.rb
@@ -0,0 +1,8 @@
+name             'docker_wordpress_test'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures docker_wordpress_test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
---
+++ docker_wordpress_test/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: docker_wordpress_test
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
