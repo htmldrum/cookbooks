d5155091fb57f265ac294dc1748acab3d659fd26
Cron Jeremy <szhukov@mshanken.com>
2014-07-07 14:52:11 -0400

    ending the cofig step to write the already hardcoded identity.php file correctly.

--- identityapi/recipes/deploy_master.rb
+++ identityapi/recipes/deploy_master.rb
@@ -37,21 +37,21 @@ end
 
 #Setting identity values
 
-#template "/var/www/identity/application/config/identity.php" do
-#  source "identity.php.erb"
-#  owner "root"
-#  group "root"
-#  mode "644"
-#end
-
-#Setting metamodel values
-
-template "/var/www/identity/application/config/metamodel.php" do
-  source "metamodel.php.erb"
+template "/var/www/identity/application/config/identity.php" do
+  source "identity.php.erb"
   owner "root"
   group "root"
   mode "644"
 end
+
+#Setting metamodel values
+
+#template "/var/www/identity/application/config/metamodel.php" do
+#  source "metamodel.php.erb"
+#  owner "root"
+#  group "root"
+#  mode "644"
+#end
 
 #Setting metamodeluser values
 
