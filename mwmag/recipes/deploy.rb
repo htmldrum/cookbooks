271c52bc8fc9357eaee8f2fdfafe74162f173e62
Cron Jeremy <szhukov@mshanken.com>
2014-08-27 15:47:47 -0400

    Dependency management via composer first commit.

--- mwmag/recipes/deploy.rb
+++ mwmag/recipes/deploy.rb
@@ -29,21 +29,21 @@ script "run rsync" do
 end
 
 
-#Adding proper node verson ppa.
+# Adding proper node verson ppa.
 execute "addapt" do
 	command "add-apt-repository ppa:chris-lea/node.js"
 	action :run
 end
 
 
-#Updating aptitude.
+# Updating aptitude.
 execute "update" do
         command "apt-get update"
         action :run
 end
 
 
-#Finally isntallign nodejs...
+# Finally isntallign nodejs...
 execute "install nodejs" do
         command "apt-get install nodejs"
         action :run
@@ -57,5 +57,27 @@ script "install_npm_dependencies" do
   cwd "/srv/www/wordpress/current/wp-content"
   code <<-EOH
   npm install && npm install bower && bower install --allow-root && npm install gulp
+  EOH
+end
+
+
+# Adding composer'd plugins via template file.
+template "/srv/www/wordpress/current/composer.json" do
+  source "composer.json.erb"
+  owner "root"
+  group "root"
+  mode "644"
+end
+
+
+# Taken from:
+# http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
+script "install_composer" do
+  interpreter "bash"
+  user "root"
+  cwd "/srv/www/wordpress/current/"
+  code <<-EOH
+  curl -s https://getcomposer.org/installer | php
+  php composer.phar install --no-dev --no-interaction --optimize-autoloader
   EOH
 end
