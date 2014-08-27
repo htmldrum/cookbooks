ddb06fcd7cf19b059f77e573fa9fcaee552c9960
Cron Jeremy <szhukov@mshanken.com>
2014-08-27 12:51:44 -0400

    more dependency management.

--- mwmag/recipes/deploy.rb
+++ mwmag/recipes/deploy.rb
@@ -1,4 +1,4 @@
-#Makes a Deploy directory.
+# Makes a Deploy directory.
 directory "/tmp/mwmag/" do
   owner "root"
   group "root"
@@ -28,12 +28,34 @@ script "run rsync" do
   EOH
 end
 
-# Running dependency isntalls.
+
+#Adding proper node verson ppa.
+execute "addapt" do
+	command "add-apt-repository ppa:chris-lea/node.js"
+	action :run
+end
+
+
+#Updating aptitude.
+execute "update" do
+        command "apt-get update"
+        action :run
+end
+
+
+#Finally isntallign nodejs...
+execute "install nodejs" do
+        command "apt-get install nodejs"
+        action :run
+end
+
+
+# Running dependency installs.
 script "install_npm_dependencies" do
   interpreter "bash"
   user "root"
   cwd "/srv/www/wordpress/current/wp-content"
   code <<-EOH
-  npm install && bower install && gulp
+  npm install && npm install bower && bower install --allow-root && npm install gulp
   EOH
 end
