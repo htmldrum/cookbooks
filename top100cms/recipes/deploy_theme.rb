6ba6a278a9bbab4d05643149c8050f8635cbae30
Cron Jeremy <szhukov@mshanken.com>
2014-10-17 14:47:51 -0400

    simple deploy recipe to update wordpress theme...

---
+++ top100cms/recipes/deploy_theme.rb
@@ -0,0 +1,20 @@
+# Grab the code for the theme from github.
+git "/tmp/wsot100" do
+  repository "git@github.com:mshanken/Top100.git"
+  revision "master"
+  action :sync
+  user "root"
+  group "root"
+end
+
+
+# Rsync that theme over to the install dir.
+
+script "run rsync" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp/wsot100/"
+  code <<-EOH
+  rsync -atv ./ /srv/www/wordpress/current/wp-content/themes/top100/
+  EOH
+end
