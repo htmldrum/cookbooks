02a88b4d556bfbc0fd24d7bbb198fd40d5f338e3
Your Name <you@example.com>
2015-01-15 15:06:21 -0500

    moved configure tasks to deploy as configure is not run

--- cellar/recipes/deploy.rb
+++ cellar/recipes/deploy.rb
@@ -21,22 +21,34 @@ node[:deploy].each do |application, deploy|
     next
   end
 
-  execute "restart elasticsearch" do
-    cwd deploy[:current_path]
-    command "sudo service elasticsearch restart"
+  execute "install ORACLE java" do
+   command "sudo add-apt-repository -y ppa:webupd8team/java && sudo apt-get update && sudo echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && sudo echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections && sudo apt-get -y install oracle-java7-installer oracle-java7-set-default"
     action :run
-    only_if do
-      File.exists?(deploy[:current_path])
-    end
   end
-
-  execute "restart worker queue" do
-    cwd deploy[:current_path]
-    command "sudo service elasticsearch restart"
+  
+  
+  #Getting the elastcisearch executable. 
+  
+  execute "wget deb package" do
+    cwd "/tmp"
+    command "wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.2.deb"
     action :run
-    only_if do
-      File.exists?(deploy[:current_path])
-    end
+  end
+  
+  #Installing the actual deb package.
+  
+  execute "install with dpkg" do
+    cwd "/tmp"
+    command "sudo dpkg -i elasticsearch-1.3.2.deb"
+    action :run
+  end
+  
+  
+  #Enabling elastic search to start
+  
+  execute "start elasticsearch" do
+    command "sudo service elasticsearch start"
+    action :run
   end
 
 end
